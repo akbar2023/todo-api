3390a535acbf7c81e4b0f63f3d24bbe31f19da94
akbar khan <39194143+akbar2023@users.noreply.github.com>
2024-06-17 11:22:37 +0200

    order METHODS

--- database.py
+++ database.py
@@ -3,24 +3,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Récupération de l'URL de la base de données
+# URL de la base de données
 DATABASE_URL = os.getenv('DATABASE_URL', "mysql+pymysql://root:@localhost:3306/todosdb")
 
-# Créer un moteur de base de données afin de gérer la connexion
+# Créer un moteur de bdd pour la connexion
 engine = create_engine(DATABASE_URL)
 
-# Créer une session locale pour interagir avec la base de données
+# Créer une session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Créer une base déclarative
 Base = declarative_base()
 
-# Définir une fonction pour obtenir une session de base de données
+# Fonction pour obtenir une session de base de données
 def get_db():
     db = SessionLocal()
     try:
         # La session de base de données est ouverte et est utilisée dans la requête
         yield db
     finally:
-        # La session de base de données est fermée après la fin de la requête
+        # Fermeture de la session de base de données a la fin de la requete
         db.close()
--- main.py
+++ main.py
@@ -8,14 +8,11 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
+@app.get("/todos", response_model=list[schemas.TodoBase])
+def read_todos(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    todos = db.query(models.Todo).offset(skip).limit(limit).all()
+    return todos
 
-@app.post("/todos/", response_model=schemas.TodoBase)
-def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
-    db_todo = models.Todo(**todo.dict())
-    db.add(db_todo)
-    db.commit()
-    db.refresh(db_todo)
-    return db_todo
 
 @app.get("/todos/{todo_id}", response_model=schemas.TodoBase)
 def read_todo(todo_id: int, db: Session = Depends(get_db)):
@@ -24,10 +21,16 @@ def read_todo(todo_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Todo not found")
     return todo
 
-@app.get("/todos/", response_model=list[schemas.TodoBase])
-def read_todos(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    todos = db.query(models.Todo).offset(skip).limit(limit).all()
-    return todos
+
+@app.post("/todos", response_model=schemas.TodoBase)
+def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    db_todo = models.Todo(**todo.dict())
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+
 
 @app.delete("/todos/{todo_id}", response_model=schemas.TodoBase)
 def delete_todo(todo_id: int, db: Session = Depends(get_db)):
